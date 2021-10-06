714f5f66859fd803d0a3b788686cbad1eb08e7f3
Htet Aung Hlaing <htetaunghlaing248@gmail.com>
2021-10-06 19:02:49 +0630

    Ex 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//Header Component
 const Header = (props) => {
   return(
     <h1>
@@ -8,6 +9,7 @@ const Header = (props) => {
   )
 }
 
+//Part Component
 const Part = (props) => {
   return(
     <p>
@@ -16,6 +18,7 @@ const Part = (props) => {
   )
 }
 
+//Content Component
 const Content = (props) => {
   return(
     <div>
@@ -26,6 +29,7 @@ const Content = (props) => {
   )
 }
 
+//Total Component
 const Total = (props) => {
   return(
     <p>
@@ -34,21 +38,28 @@ const Total = (props) => {
   )
 }
 
+//Main App
 const App = () => {
   //const definitions
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name : 'Fundamentals of React',
+    exercises : 10
+  }
+  const part2 = {
+    name : 'Using props to pass the data',
+    exercises : 7
+  }
+  const part3 = {
+    name : 'State of a component',
+    exercises : 14
+  }
 
   return(
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises}/>
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </div>
   )
 }
