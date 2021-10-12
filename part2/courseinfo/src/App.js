9c324d939d2eedc3c80e5c4cb5536a4c2762d840
Htet Aung Hlaing <htetaunghlaing248@gmail.com>
2021-10-12 21:01:31 +0630

    Ex 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -9,7 +9,7 @@ const Header = (props) => {
 
 //Total component
 const Total = (props) => {
-  const total = props.parts.reduce((sum ,p) => sum + p.exercises, 0 )
+  const total = props.parts.reduce((sum ,part) => sum + part.exercises, 0 )
   return(
     <strong>total of {total} exercises</strong>
   ) 
