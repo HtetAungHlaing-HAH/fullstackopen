39955e276d2e6ec68bfc6982b8788cd037b2425e
Htet Aung Hlaing <htetaunghlaing248@gmail.com>
2021-10-14 22:13:41 +0630

    Ex 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,13 +4,17 @@ import Persons from './components/Persons'
 //main App component
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Arto Something', number: '3233234', id: 2 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 3 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 4 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 5 }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ search, setSearch ] = useState('')
+  const namesToShow = search.length === 0
+  ? persons : persons.filter(person => person.name.search(new RegExp(search, "i")) === 0)
 
   //to store new name in input element
   const handleNewName = (event) => {
@@ -22,12 +26,18 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  //to store search name in input element
+  const handleSearchName = (event) => {
+    setSearch(event.target.value)
+  }
+
   //to add new name in persons
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id : persons.length +1
     }
     const nameFound = persons.find(person => person.name === nameObject.name)
     if(nameFound)
@@ -45,6 +55,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input value={search} onChange={handleSearchName} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNewName}/>
@@ -57,7 +71,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Persons key={person.name} person={person} />)}
+      {namesToShow.map(person => <Persons key={person.id} person={person} />)}
     </div>
   )
 }
