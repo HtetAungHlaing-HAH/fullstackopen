5270ff1b9c51665cfab8253b8eaae9399780b9cb
Htet Aung Hlaing <htetaunghlaing248@gmail.com>
2021-10-16 01:00:41 +0630

    Ex 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 
 
 //main App component
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Arto Something', number: '3233234', id: 2 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 3 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 4 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 5 }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ search, setSearch ] = useState('')
+
+  //effect-hook function reference
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  //effect-hook
+  useEffect(hook, [])
+
+  console.log('render', persons.length, 'persons')
 
   //to store filtered names by search field
   const namesToShow = search.length === 0
